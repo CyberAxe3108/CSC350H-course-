4793dc69d3a408b0b8b551e13c64c46ce156403f
CyberAxe3108 <125237722+CyberAxe3108@users.noreply.github.com>
2026-02-02 21:48:18 -0500

    Unit 2: .NET Library
    Unit 2: Call the methods of the .NET class library
    This code simulates a dice roll using the Random.Next() method to generate a number, and the Console.WriteLine() method to display the value.*/
    Overloading methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+﻿//Unit 2: Call the methods of the .NET class library
+/*This code simulates a dice roll using the Random.Next() 
+method to generate a number, 
+and the Console.WriteLine() method to display the value.*/
+//Overloading methods
+using System.Numerics;
+
+Random dice = new Random();
+int roll1  = dice.Next();
+int roll2  = dice.Next(101);
+int roll3  = dice.Next(50, 101);
+
+Console.WriteLine(roll1);
+Console.WriteLine(roll2);
+Console.WriteLine(roll3);
+
+//Using the Math class
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
