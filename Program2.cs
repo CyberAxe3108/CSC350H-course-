37bd05c6df6c2754ac3650f3b32e1b9161a89d70
CyberAxe3108 <125237722+CyberAxe3108@users.noreply.github.com>
2026-02-02 22:01:12 -0500

    Unit 3: Random Dice Game
    How to use methods from the Random class to generate
    Math classes in C#
    Also, how to use conditional statements to control program flow

---
+++ Program2.cs
@@ -0,0 +1,82 @@
+﻿//Unit 3: Random Dice Game
+//How to use methods from the Random class to generate
+// Math classes in C#
+// Also how to use conditional statements to control program flow
+
+
+Random dice = new Random();
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+
+roll1 = 6;
+roll2 = 6;
+roll3 = 6;
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice rolls: {roll1} + {roll2} + {roll3} = {total}");
+
+
+if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if (roll1 == roll2 && roll2 == roll3)
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+    else
+    {
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+    }
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+else
+{
+    Console.WriteLine("You lose!");
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}else if(total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}else if(total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}else
+{
+    Console.WriteLine("You win a kitten!");
+}
+
+
+//Unit 3: Subscription Renewal Offer
+//Using Random class and conditional statements
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine($"Your subscription expires soon. Renew now!");
+}else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration}days!");
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires within a day!");
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}else
+{
+    Console.WriteLine($"Your subscription has expired.");
+}
